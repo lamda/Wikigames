42866e59d30d428b69c558b0ded69a8e5e9854fe
lamda <daniel.lamprecht@gmx.at>
2014-12-10 10:49:09 +0100

    added a plain text extractor

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 from __future__ import division, print_function
 from collections import defaultdict
+import cPickle as pickle
+import HTMLParser
 import io
 import os
 import pdb
 import re
-import urllib
 import urllib2
 
 # import graph_tool.all as gt
@@ -145,10 +146,37 @@ class LogisticRegressor(object):
 
 class Network(object):
     def __init__(self, filename, graph_tool=False):
+        # read the graph
         if graph_tool:
             self.graph = self.read_edge_list_gt(filename)
         else:
             self.graph = self.read_edge_list_nx(filename)
+
+        # build some mappings from the database
+        db_connector = DbConnector()
+        pages = db_connector.execute('SELECT * FROM pages')
+        self.id2title = {p['id']: p['name'] for p in pages}
+        self.id2name = {p['id']:
+                   re.findall(r'\\([^\\]*?)\.htm', p['link'])[0] for p in pages}
+        self.name2id = {v: k for k, v in self.id2name.items()}
+
+        games = db_connector.execute("""SELECT * FROM games
+                                     WHERE `game_name` LIKE 'PLAIN%'""")
+        self.game2start_target = {v['game_name']:
+                                  (self.id2name[v['start_page_id']],
+                                   self.id2name[v['goal_page_id']])
+                                  for v in games}
+
+        nodes = pages = db_connector.execute('SELECT * FROM node_data')
+        self.id2deg = {p['id']: p['degree'] for p in nodes}
+        self.id2pr = {p['id']: p['pagerank'] for p in nodes}
+
+        ngrams = db_connector.execute('SELECT * FROM ngrams')
+        self.id2ngram = {p['id']: p['probability'] for p in ngrams}
+
+        links = db_connector.execute('''SELECT page_id, SUM(amount) as links
+                                     FROM links GROUP BY page_id;''')
+        self.id2links = {p['page_id']: int(p['links']) for p in links}
 
     def read_edge_list_gt(self, filename, directed=True, parallel_edges=False):
         graph = gt.Graph(directed=directed)
@@ -190,27 +218,10 @@ class Network(object):
 
 
 def main():
-    # nw = Network('data/links.txt')
-    # graph = nw.graph
+    # build the network
+    nw = Network('data/links.txt')
 
-    db_connector = DbConnector()
-    pages = db_connector.execute('SELECT * FROM pages')
-    id2title = {p['id']: p['name'] for p in pages}
-    id2name = {p['id']:
-               re.findall(r'\\([^\\]*?)\.htm', p['link'])[0] for p in pages}
-    name2id = {v: k for k, v in id2name.items()}
-
-    nodes = pages = db_connector.execute('SELECT * FROM node_data')
-    id2deg = {p['id']: p['degree'] for p in nodes}
-    id2pr = {p['id']: p['pagerank'] for p in nodes}
-
-    ngrams = db_connector.execute('SELECT * FROM ngrams')
-    id2ngram = {p['id']: p['probability'] for p in ngrams}
-
-    links = db_connector.execute('''SELECT page_id, SUM(amount) as links
-                                 FROM links GROUP BY page_id;''')
-    id2links = {p['page_id']: int(p['links']) for p in links}
-
+    # load or compute the click data as a pandas frame
     try:  # load the precomputed click data
         clicks = pd.read_pickle('clicks.pd')
     except IOError:  # compute click data
@@ -221,43 +232,51 @@ def main():
         results = []
         folder_logs = 'data/logfiles/'
         for folder in sorted(os.listdir(folder_logs)):
-            print(folder)
+            print('\n', folder)
             files = sorted([f for f in os.listdir(folder_logs + folder)
                             if f.startswith('PLAIN')])
             for filename in files:
+                print('   ', filename)
                 fname = folder_logs + folder + '/' + filename
                 df = pd.read_csv(fname, header=None, usecols=[1, 2, 3],
                                  names=['time', 'action', 'node'],
                                  infer_datetime_format=True, sep='\t')
                 if df['action'].value_counts()['GAME_STARTED'] > 1:
-                    print('Error: duplicated game start, dropping',
+                    print('        Error: duplicated game start, dropping',
                           folder, fname)
                     continue
+
                 # ld = df[df['action'] == 'link_data']
                 # ld['node'] = ld['node'].apply(lambda k: int(re.findall(r"link_nr': (\d+)", k)[0]))
-                success = df.iloc[-1]['action'] == 'GAME_COMPLETED'
-                if success:
+
+                if df.iloc[-1]['action'] == 'GAME_COMPLETED':
                     idx = -1
                     while df.iloc[idx]['action'] != 'link_data':
                         idx -= 1
                         if idx < -df.shape[0]:
-                            print('Error: no link_data entry found')
+                            print('        Error: no link_data entry found')
                             pdb.set_trace()
                     df.loc[df.index[-1] + 1] = [df['time'].max(), 'load',
                                                 df.iloc[idx]['node']]
-                    df['success'] = pd.Series(np.ones(df.shape[0]), index=df.index)
+                    df['success'] = pd.Series(np.ones(df.shape[0]),
+                                              index=df.index)
                 else:
-                    df['success'] = pd.Series(np.zeros(df.shape[0]), index=df.index)
+                    df['success'] = pd.Series(np.zeros(df.shape[0]),
+                                              index=df.index)
                 df = df[df['action'] == 'load']
+                if df.shape[0] < 2:
+                    print('        Error: game too short, dropping',
+                          folder, fname)
+                    continue
                 df['node'] = df['node'].apply(parse_node)
                 df.index = np.arange(len(df))
                 try:
-                    df['node_id'] = [name2id[n] for n in df['node']]
-                    df['degree'] = [id2deg[i] for i in df['node_id']]
-                    df['pagerank'] = [id2pr[i] for i in df['node_id']]
-                    df['ngram'] = [id2ngram[i] for i in df['node_id']]
+                    df['node_id'] = [nw.name2id[n] for n in df['node']]
+                    df['degree'] = [nw.id2deg[i] for i in df['node_id']]
+                    df['pagerank'] = [nw.id2pr[i] for i in df['node_id']]
+                    df['ngram'] = [nw.id2ngram[i] for i in df['node_id']]
                 except KeyError, e:
-                    print('Error: key not found', folder, fname, e)
+                    print('        Error: key not found', folder, fname, e)
                     continue
 
                 df.iloc[:, 0] = df.iloc[:, 0] - df.iloc[:, 0].min()
@@ -274,16 +293,84 @@ def main():
                 # linkpos = ld['node'] / [id2links[i] for i in df['node_id']]
                 # df['linkpos'] = [v for v in linkpos]
                 results.append(df)
+                match = re.findall(r'(PLAIN_[\d]+_[a-z0-9_\-]+)\.', filename)
+                if not match:
+                    print('        Error: game mission not matched')
+                    pdb.set_trace()
+                else:
+                    match = match[0]
+                start, target = nw.game2start_target[match]
+
+                if not start == df['node'].iloc[0] and\
+                        target == df['node'].iloc[-1]:
+                    print('        Error: start or target not present')
+                    print(df)
+                # pdb.set_trace()
 
         clicks = pd.concat(results, ignore_index=True)
         clicks['intercept'] = 1.0
         clicks.to_pickle('clicks.pd')
 
-    train_cols = ['time', 'degree', 'pagerank', 'ngram', 'intercept'] #, 'linkpos']
+    pdb.set_trace()
+    # run logistic regression
+    train_cols = ['time', 'degree', 'pagerank', 'ngram', 'intercept']  #, 'linkpos']
     regressor = LogisticRegressor()
     regressor.regress(clicks['success'], clicks[train_cols])
     pdb.set_trace()
 
 
+def compute_link_positions():
+    nw = Network('data/links.txt')
+
+    class MLStripper(HTMLParser.HTMLParser):
+        def __init__(self):
+            HTMLParser.HTMLParser.__init__(self)
+            self.reset()
+            self.fed = []
+
+        def handle_data(self, d):
+            self.fed.append(d)
+
+        def get_data(self):
+            return ''.join(self.fed)
+
+        def reset(self):
+            self.fed = []
+            HTMLParser.HTMLParser.reset(self)
+
+    parser = MLStripper()
+    link_regex = re.compile('(<a href="../../wp/[^/]+/(.+?)\.htm" ' +
+                             'title="[^"]+">.+?</a>)' +
+                            '|' +
+                            '(<area.+?href=' +
+                            '"../../wp/[^/]+/(.+?)\.htm".+?/>)')
+    folder = 'data/wiki-schools/wp/'
+    plaintext_dir = 'data/wiki-schools/plaintext/'
+    if not os.path.exists(plaintext_dir):
+        os.makedirs(plaintext_dir)
+    for i, a in enumerate(nw.name2id.keys()):
+        print(unicode(i+1) + '/' + unicode(len(nw.name2id)))
+        fname = folder + a[0].lower() + '/' + a + '.htm'
+        with io.open(fname, encoding='utf-8') as infile:
+            data = infile.read()
+        data = data.split('<!-- start content -->')[1]
+        data = data.split('<div class="printfooter">')[0]
+        data = [d.strip() for d in data.splitlines()]
+        data = [d for d in data if d]
+        data = data[1:]  # skip background information about W4S
+        text = []
+        for d in data:
+            parser.reset()
+            parser.feed(parser.unescape(d))
+            stripped_d = parser.get_data()
+            if stripped_d:
+                text.append(stripped_d)
+        text = '\n'.join(text)
+        with io.open(plaintext_dir + a + '.txt', 'w', encoding='utf-8') as outfile:
+            outfile.write(text)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    compute_link_positions()
+    pdb.set_trace()
