1da04b427da4f7ecff43eece19c928ec851823d0
Daniel Lamprecht <daniel.lamprecht@tugraz.at>
2015-07-10 10:08:12 +0200

    adapted the clickmodels to the new dataframe

--- clickmodel.py
+++ clickmodel.py
@@ -30,6 +30,10 @@ class ClickModel(object):
     def __init__(self, df):
         self.df = df
         self.data = collections.defaultdict(float)
+        self.keys = set(df['source'])
+        self.grouped = df.groupby('source')
+        self.total_clicks = {k: self.grouped.get_group(k)['amount'].sum()
+                             for k in self.keys}
 
     def normalize(self):
         total = sum(self.data.values())
@@ -40,31 +44,27 @@ class GroundTruthModel(ClickModel):
     def __init__(self, df):
         self.label = 'GroundTruth'
         super(GroundTruthModel, self).__init__(df)
-        # TODO: sum over targets
-        self.data = {k: v
-                     for k, v in df['node_next'].value_counts().iteritems()}
+        self.data = {k: v for
+                     k, v in df.groupby('target')['amount'].sum().iteritems()}
         self.normalize()
         print('click data for %d pages' % len(self.data))
 
     def compare(self, mdl):
         kl = np.abs(scipy.stats.entropy(self.data, mdl.data, base=2))
         lab = self.label + '\t' + mdl.label
-        print('\t%.2f\t%s' % (kl, lab))
+        print('\t%.4f\t%s' % (kl, lab))
 
 
 class UniformModel(ClickModel):
     def __init__(self, df):
         super(UniformModel, self).__init__(df)
         self.label = 'Uniform'
-        keys = df['node'].value_counts().index
-        grouped = df.groupby('node')
-        for kidx, key in enumerate(keys):
-            print(kidx, '/', len(keys), end='\r')
-            df_sub = grouped.get_group(key)
-            targets = set(df_sub['node_next'])
-            pdb.set_trace()
-            for t in targets:
-                self.data[t] += len(df_sub) / len(targets)
+        for kidx, key in enumerate(self.keys):
+            print(kidx+1, '/', len(self.keys), end='\r')
+            df_sub = self.grouped.get_group(key)
+            targets = len(df_sub['target'])
+            for ridx, row in df_sub.iterrows():
+                self.data[row['target']] += self.total_clicks[key]/targets
         self.normalize()
 
 
@@ -72,76 +72,72 @@ class IndirectProportionModel(ClickModel):
     def __init__(self, df):
         super(IndirectProportionModel, self).__init__(df)
         self.label = 'Indirect Proportional'
-        keys = df['node'].value_counts().index
-        grouped = df.groupby('node')
-        for kidx, key in enumerate(keys):
-            print(kidx, '/', len(keys), end='\r')
-            df_sub = grouped.get_group(key)
+        for kidx, key in enumerate(self.keys):
+            print(kidx+1, '/', len(self.keys), end='\r')
+            df_sub = self.grouped.get_group(key)
             node2p = collections.defaultdict(float)
             for ridx, row in df_sub.iterrows():
-                node2p[row['node_next']] += 1 / (row['linkpos_first'] + 1)
-                if row['ambiguous']:
-                    node2p[row['node_next']] += 1 / (row['linkpos_last'] + 1)
+                node2p[row['target']] += 1 / (row['linkpos_first'] + 1)
+                if row['linkpos_ambig']:
+                    node2p[row['target']] += 1 / (row['linkpos_last'] + 1)
             total = sum(node2p.values())
             for k, v in node2p.items():
-                self.data[k] += (v * len(df_sub)) / total
-
+                self.data[k] += self.total_clicks[key] * v / total
         self.normalize()
 
 
 class InverseRankModel(ClickModel):
     def __init__(self, df):
         super(InverseRankModel, self).__init__(df)
-        self.label = 'Inverse Rank'
-        keys = df['node'].value_counts().index
-        grouped = df.groupby('node')
-        for kidx, key in enumerate(keys):
-            print(kidx, '/', len(keys), end='\r')
-            df_sub = grouped.get_group(key)
+        self.label = 'Inverse Rank'  # similar to Zipf's law
+        for kidx, key in enumerate(self.keys):
+            print(kidx+1, '/', len(self.keys), end='\r')
+            df_sub = self.grouped.get_group(key)
             nodepos = set()
             for ridx, row in df_sub.iterrows():
-                nodepos.add((row['node_next'], row['linkpos_first'] + 1))
-                if row['ambiguous']:
-                    nodepos.add((row['node_next'], row['linkpos_last'] + 1))
-            total = sum(range(1, len(nodepos)+1)) + len(nodepos)
+                nodepos.add((row['linkpos_first'] + 1, row['target']))
+                if row['linkpos_ambig']:
+                    nodepos.add((row['linkpos_last'] + 1, row['target']))
+            total = sum(1/(idx+1) for idx in range(len(nodepos)))
             for idx, e in enumerate(sorted(nodepos)):
-                self.data[e[0]] += ((idx+1) * len(df_sub)) / total
+                self.data[e[1]] += self.total_clicks[key] * (1/(idx+1)) / total
         self.normalize()
 
 
 class InverseModel(ClickModel):
     def __init__(self, df):
         super(InverseModel, self).__init__(df)
-        self.label = 'Test'
-        keys = df['node'].value_counts().index
-        grouped = df.groupby('node')
-        for kidx, key in enumerate(keys):
-            print(kidx, '/', len(keys), end='\r')
-            df_sub = grouped.get_group(key)
+        self.label = 'InverseModel'
+        for kidx, key in enumerate(self.keys):
+            print(kidx, '/', len(self.keys), end='\r')
+            df_sub = self.grouped.get_group(key)
             node2p = collections.defaultdict(float)
             for ridx, row in df_sub.iterrows():
-                node2p[row['node_next']] += row['word_count'] - row['linkpos_first']
-                if row['ambiguous']:
-                    node2p[row['node_next']] += row['word_count'] - row['linkpos_last']
+                rt = row['target']
+                node2p[rt] += row['word_count'] - row['linkpos_first']
+                if row['linkpos_ambig']:
+                    node2p[rt] += row['word_count'] - row['linkpos_last']
             total = sum(node2p.values())
             for k, v in node2p.items():
-                self.data[k] += (v * len(df_sub)) / total
+                self.data[k] += v * self.total_clicks[key] / total
         self.normalize()
 
 
-def get_df_w4s():
+def get_df_w4s(wikti=False):
     # includes both successful and unsuccessful games, all for spl = 3
-    path = os.path.join('data', 'WIKTI', 'data.obj')
-    # path = os.path.join('data', 'Wikispeedia', 'data.obj')
+    if wikti:
+        path = os.path.join('data', 'WIKTI', 'data.obj')
+    else:
+        path = os.path.join('data', 'Wikispeedia', 'data.obj')
     df = pd.read_pickle(path)
     df = df[['node', 'node_id', 'linkpos_first', 'linkpos_last',
-             'backtrack', 'subject']]
+             'backtrack', 'subject', 'word_count']]
     df['target'] = df['node'].shift(-1)
     df['target_id'] = df['node_id'].shift(-1)
     df.columns = ['source', 'source_id'] + df.columns[2:].tolist()
     df = df[~df['backtrack']]
     df = df[['source', 'source_id', 'linkpos_first', 'linkpos_last',
-             'target', 'target_id']]
+             'word_count', 'target', 'target_id']]
     df['linkpos_ambig'] = df['linkpos_first'] != df['linkpos_last']
     df = df.dropna()
     df['amount'] = df.groupby(['source', 'target']).transform('count')['source_id']
@@ -155,15 +151,16 @@ def get_df_wikipedia():
     df = pd.read_pickle(path).values()[0]
     return df
 
+
 if __name__ == '__main__':
-    df = get_df_w4s()
+    df = get_df_w4s(wikti=False)
     # df = get_df_wikipedia()
     gt = GroundTruthModel(df)
 
     models = [
         UniformModel,
-        # IndirectProportionModel,
-        # InverseRankModel,
+        IndirectProportionModel,
+        InverseRankModel,
         InverseModel,
     ]
     models = [m(df) for m in models]
